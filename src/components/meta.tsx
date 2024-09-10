5af1363255a07c933c8efda9aef4554dc6e21eac
Takaaki Inada <takaaki.inada@gmail.com>
2024-09-11 07:17:45 +0900

    update 9/11 slides

--- src/components/meta.tsx
+++ src/components/meta.tsx
@@ -1,10 +1,9 @@
 import Head from "next/head";
 export const Meta = () => {
-  const title = "ChatVRM";
+  const title = "株式会社ずんだもんAI放送局まもなく放送100回記念！";
   const description =
-    "Webブラウザだけで3Dキャラクターとの会話を、マイクやテキスト入力、音声合成を用いて楽しめます。";
-  // TODO: ogp画像を用意して、URLを指定する
-  const imageUrl = "https://zund-arm-on.com//images/zundarmon_artwork_3000_2.jpg";
+    "あなたも番組に参加して気分はコメンテーター！？<br/>配信ネタに困っているVTuberさんもネタを提供してくれてコメントするだけで良い最終兵器として大助かり！<br/>Webブラウザだけでずんだもんとの会話を、マイクやテキスト入力、音声合成を用いて楽しめます。";
+  const imageUrl = "https://zund-arm-on.com/images/zund_arm_on_100_notice.png";
   return (
     <Head>
       <title>{title}</title>
