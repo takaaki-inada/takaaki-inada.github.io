a8a44e33940859608d8a2e511c5bd0b4a94e3a70
Takaaki Inada <takaaki.inada@gmail.com>
2024-10-04 08:58:33 +0900

    public/slidesにあるディレクトリを参照して、pathsをリストでパスを生成するお試し

--- src/pages/programs/[id]/index.tsx
+++ src/pages/programs/[id]/index.tsx
@@ -17,19 +17,36 @@ export default function DynamicPage({ id }: { id: string }) {
 }
 
 export async function getStaticPaths() {
+  // public/slidesにあるディレクトリを参照して、pathsをリストでパスを生成する
+  const fs = require('fs');
+  const path = require('path');
+  const slidesDir = path.join(process.cwd(), 'public/slides');
+  const slideDirs = fs.readdirSync(slidesDir);
+  const paths = slideDirs.map((slideDir: string) => {
+    return {
+      params: {
+        id: slideDir,
+      },
+    }
+  })
   return {
-     // Add your paths here
-    paths: [
-      { params: { id: '20240827' } },
-      { params: { id: '20240830' } },
-      { params: { id: '20240903' } },
-      { params: { id: '20240910' } },
-      { params: { id: '20240911' } },
-      { params: { id: '20241002ex' } },
-      { params: { id: 'LT_podcast' } },
-    ],
+    paths: paths,
     fallback: false,
   };
+  // return {
+  //   // Add your paths here
+  //   paths: [
+  //     { params: { id: '20240827' } },
+  //     { params: { id: '20240830' } },
+  //     { params: { id: '20240903' } },
+  //     { params: { id: '20240910' } },
+  //     { params: { id: '20240911' } },
+  //     { params: { id: '20241002ex' } },
+  //     { params: { id: '20241004ex' } },
+  //     { params: { id: 'LT_podcast' } },
+  //   ],
+  //   fallback: false,
+  // };
 }
 
 export async function getStaticProps({ params }: { params: { id: string } }) {
