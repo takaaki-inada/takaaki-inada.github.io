c8251c59834f2f6b82fc119d59c1651e0a25a775
Takaaki Inada <takaaki.inada@gmail.com>
2025-02-10 23:36:22 +0900

    [WIP] head.htmlにpage.cardを渡せるか確認

--- src/components/meta.tsx
+++ src/components/meta.tsx
@@ -7,14 +7,11 @@ export const Meta = () => {
   return (
     <Head>
       <title>{title}</title>
+      <meta name="twitter:card" content="summary_large_image" />
       <meta name="description" content={description} />
       <meta property="og:title" content={title} />
       <meta property="og:description" content={description} />
       <meta property="og:image" content={imageUrl} />
-      <meta name="twitter:card" content="summary_large_image" />
-      <meta name="twitter:title" content={title} />
-      <meta name="twitter:description" content={description} />
-      <meta name="twitter:image" content={imageUrl} />
     </Head>
   );
 };
